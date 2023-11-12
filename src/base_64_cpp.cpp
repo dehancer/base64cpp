0bbde5b32d931d414c018e78836294e4e4ba6531
denn lenovo <denn.nevera@gmail.com>
2023-11-12 15:35:04 +0400

    code cleaned up

--- src/base_64_cpp.cpp
+++ src/base_64_cpp.cpp
@@ -93,7 +93,6 @@ namespace base64 {
 
       unsigned char in[4], out[3], v;
       int i, len;
-      int osize=0;
       int j=0;
 
       auto* indata = reinterpret_cast<const unsigned char*>(input.data());
